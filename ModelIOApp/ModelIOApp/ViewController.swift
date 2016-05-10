f0ef947ceefa67109555ced5b9f0a461beaefc56
iosdevzone <idz@iosdeveloperzone.com>
2016-05-09 21:32:21 -0700

    Starting poing for tutorial.

---
+++ ModelIOApp/ModelIOApp/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  ModelIOApp
+//
+//  Created by idz on 5/9/16.
+//  Copyright © 2016 iOS Developer Zone. 
+//  License: MIT
+//  See: https://raw.githubusercontent.com/iosdevzone/GettingStartedWithModelIO/master/LICENSE
+//
+
+import UIKit
+import ModelIO
+import SceneKit
+import SceneKit.ModelIO
+
+
+class ViewController: UIViewController {
+    
+    var sceneView: SCNView {
+        return self.view as! SCNView
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
